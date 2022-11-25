59249b12a153c2d517bbf3cccd09e4187fa8b40a
Luis Eduardo <luiseduess@gmail.com>
2022-11-24 22:34:09 -0300

    JS in Progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
 new class TipCalculator {
   constructor() {
+    this.tip = 0
+
     this.initialize()
   }
 
   initialize() {
+    this.tipSelector()
+  }
 
+  tipSelector() {
+    document.querySelectorAll('.btn-percentage').forEach(el => {
+      el.addEventListener('click', event => {
+        this.tip = Number(event.target.innerHTML.replace(/%/g, '')) / 100
+      })
+    })
   }
 }
